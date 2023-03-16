107478debb04ce4665a3ac211068609d332c09b1
JHyun Ahn <jhyunahn226@gmail.com>
2023-03-16 22:39:31 +0900

    #5.1 Interests Screen - Wrap

--- lib/features/authentication/login_forms_screen.dart
+++ lib/features/authentication/login_forms_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 import 'package:tiktok/features/authentication/widgets/form_button.dart';
+import 'package:tiktok/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -14,11 +15,14 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   Map<String, String> formData = {};
 
-  void _onnSubmitTap() {
+  void _onSubmitTap() {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         //각 TextFormField의 validate를 모두 통과하면
         _formKey.currentState!.save(); //각 TextFormField의 onSaved를 호출
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const InterestsScreen(),
+        ));
       }
     }
   }
@@ -41,6 +45,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                   hintText: 'Email',
                 ),
                 validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Please write your email";
+                  }
                   return null;
                 },
                 onSaved: (newValue) {
@@ -55,6 +62,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                   hintText: 'Password',
                 ),
                 validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Please write your password";
+                  }
                   return null;
                 },
                 onSaved: (newValue) {
@@ -65,7 +75,7 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
               ),
               Gaps.v28,
               GestureDetector(
-                onTap: _onnSubmitTap,
+                onTap: _onSubmitTap,
                 child: const FormButton(disabled: false),
               ),
             ],
