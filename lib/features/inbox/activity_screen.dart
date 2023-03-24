5d552eac0174720ac1948df2f1c2ace18bb8b61e
JHyun Ahn <jhyunahn226@gmail.com>
2023-03-25 00:33:40 +0900

    #10.5 SlideTransition

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -14,14 +14,46 @@ class _ActivityScreenState extends State<ActivityScreen>
     with SingleTickerProviderStateMixin {
   final List<String> _notifications = List.generate(20, (index) => '${index}h');
 
+  final List<Map<String, dynamic>> _tabs = [
+    {
+      "title": "All activity",
+      "icon": FontAwesomeIcons.solidMessage,
+    },
+    {
+      "title": "Likes",
+      "icon": FontAwesomeIcons.solidHeart,
+    },
+    {
+      "title": "Comments",
+      "icon": FontAwesomeIcons.solidComments,
+    },
+    {
+      "title": "Mentions",
+      "icon": FontAwesomeIcons.at,
+    },
+    {
+      "title": "Followers",
+      "icon": FontAwesomeIcons.solidUser,
+    },
+    {
+      "title": "From TikTok",
+      "icon": FontAwesomeIcons.tiktok,
+    }
+  ];
+
   late final AnimationController _animationController = AnimationController(
     vsync: this,
     duration: const Duration(milliseconds: 200),
   ); //late이 붙어야 'this'를 참조할 수 있음
 
-  late final Animation<double> _animation = Tween(
+  late final Animation<double> _arrowAnimation = Tween(
     begin: 0.0,
     end: 0.5,
+  ).animate(_animationController);
+
+  late final Animation<Offset> _panelAnimation = Tween(
+    begin: const Offset(0, -1),
+    end: const Offset(0, 0),
   ).animate(_animationController);
 
   void _onTitleTap() {
@@ -50,7 +82,7 @@ class _ActivityScreenState extends State<ActivityScreen>
               const Text('All Activity'),
               Gaps.h2,
               RotationTransition(
-                turns: _animation,
+                turns: _arrowAnimation,
                 child: const FaIcon(
                   FontAwesomeIcons.chevronDown,
                   size: Sizes.size14,
@@ -60,106 +92,151 @@ class _ActivityScreenState extends State<ActivityScreen>
           ),
         ),
       ),
-      body: ListView(
+      body: Stack(
         children: [
-          Gaps.v14,
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: Sizes.size16,
-            ),
-            child: Text(
-              'New',
-              style: TextStyle(
-                fontSize: Sizes.size14,
-                color: Colors.grey.shade500,
-              ),
-            ),
-          ),
-          Gaps.v14,
-          for (var notification in _notifications)
-            Dismissible(
-              key: Key(notification),
-              onDismissed: (direction) => _onDismissed(notification, direction),
-              background: Container(
-                alignment: Alignment.centerLeft,
-                color: Colors.green,
-                child: const Padding(
-                  padding: EdgeInsets.only(
-                    left: Sizes.size10,
-                  ),
-                  child: FaIcon(
-                    FontAwesomeIcons.checkDouble,
-                    color: Colors.white,
-                    size: Sizes.size32,
+          ListView(
+            children: [
+              Gaps.v14,
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size16,
+                ),
+                child: Text(
+                  'New',
+                  style: TextStyle(
+                    fontSize: Sizes.size14,
+                    color: Colors.grey.shade500,
                   ),
                 ),
               ),
-              secondaryBackground: Container(
-                alignment: Alignment.centerRight,
-                color: Colors.red,
-                child: const Padding(
-                  padding: EdgeInsets.only(
-                    right: Sizes.size10,
-                  ),
-                  child: FaIcon(
-                    FontAwesomeIcons.trashCan,
-                    color: Colors.white,
-                    size: Sizes.size32,
-                  ),
-                ),
-              ),
-              child: ListTile(
-                minVerticalPadding: Sizes.size16,
-                leading: Container(
-                  width: Sizes.size52,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    border: Border.all(
-                      color: Colors.grey.shade400,
-                      width: Sizes.size1,
+              Gaps.v14,
+              for (var notification in _notifications)
+                Dismissible(
+                  key: Key(notification),
+                  onDismissed: (direction) =>
+                      _onDismissed(notification, direction),
+                  background: Container(
+                    alignment: Alignment.centerLeft,
+                    color: Colors.green,
+                    child: const Padding(
+                      padding: EdgeInsets.only(
+                        left: Sizes.size10,
+                      ),
+                      child: FaIcon(
+                        FontAwesomeIcons.checkDouble,
+                        color: Colors.white,
+                        size: Sizes.size32,
+                      ),
                     ),
                   ),
-                  child: const Center(
-                    child: FaIcon(
-                      FontAwesomeIcons.bell,
-                      color: Colors.black,
+                  secondaryBackground: Container(
+                    alignment: Alignment.centerRight,
+                    color: Colors.red,
+                    child: const Padding(
+                      padding: EdgeInsets.only(
+                        right: Sizes.size10,
+                      ),
+                      child: FaIcon(
+                        FontAwesomeIcons.trashCan,
+                        color: Colors.white,
+                        size: Sizes.size32,
+                      ),
                     ),
                   ),
-                ),
-                title: RichText(
-                  text: TextSpan(
-                    text: 'Account updates:',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
-                      fontSize: Sizes.size16,
+                  child: ListTile(
+                    minVerticalPadding: Sizes.size16,
+                    leading: Container(
+                      width: Sizes.size52,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.grey.shade400,
+                          width: Sizes.size1,
+                        ),
+                      ),
+                      child: const Center(
+                        child: FaIcon(
+                          FontAwesomeIcons.bell,
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
-                    children: [
-                      TextSpan(
-                        text: ' Upload longer videos',
+                    title: RichText(
+                      text: TextSpan(
+                        text: 'Account updates:',
                         style: const TextStyle(
-                          fontWeight: FontWeight.normal,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                          fontSize: Sizes.size16,
                         ),
                         children: [
                           TextSpan(
-                            text: ' $notification',
-                            style: TextStyle(
+                            text: ' Upload longer videos',
+                            style: const TextStyle(
                               fontWeight: FontWeight.normal,
-                              color: Colors.grey.shade500,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: ' $notification',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.grey.shade500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    trailing: const FaIcon(
+                      FontAwesomeIcons.chevronRight,
+                      size: Sizes.size16,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+          SlideTransition(
+            position: _panelAnimation,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(
+                    Sizes.size5,
+                  ),
+                  bottomRight: Radius.circular(
+                    Sizes.size5,
+                  ),
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  for (var tab in _tabs)
+                    ListTile(
+                      title: Row(
+                        children: [
+                          FaIcon(
+                            tab["icon"],
+                            color: Colors.black,
+                            size: Sizes.size16,
+                          ),
+                          Gaps.h20,
+                          Text(
+                            tab["title"],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-                trailing: const FaIcon(
-                  FontAwesomeIcons.chevronRight,
-                  size: Sizes.size16,
-                ),
+                    ),
+                ],
               ),
             ),
+          ),
         ],
       ),
     );
