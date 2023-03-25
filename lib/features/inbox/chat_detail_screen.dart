bcab23a07e8151df0e8a0f944c65069af8075fda
JHyun Ahn <jhyunahn226@gmail.com>
2023-03-26 01:24:17 +0900

    #11.4 Chat Detail

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/constants/sizes.dart';
 
 class ChatDetailScreen extends StatefulWidget {
   const ChatDetailScreen({super.key});
@@ -10,8 +11,202 @@ class ChatDetailScreen extends StatefulWidget {
 }
 
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  bool _isWriting = false;
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
+  }
+
+  void _onStopWriting() {
+    FocusScope.of(context).unfocus();
+    _isWriting = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: ListTile(
+          contentPadding: EdgeInsets.zero,
+          horizontalTitleGap: Sizes.size8,
+          leading: Stack(
+            children: [
+              const CircleAvatar(
+                radius: Sizes.size20,
+                foregroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/23349047?v=4'),
+                child: Text('JH'),
+              ),
+              Positioned(
+                bottom: -Sizes.size2,
+                right: 0,
+                child: Container(
+                  width: Sizes.size16,
+                  height: Sizes.size16,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: Sizes.size2,
+                    ),
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          title: const Text(
+            'Lynn',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          subtitle: const Text('Active now'),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              FaIcon(
+                FontAwesomeIcons.flag,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              Gaps.h32,
+              FaIcon(
+                FontAwesomeIcons.ellipsis,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: _onStopWriting,
+        child: Stack(
+          children: [
+            ListView.separated(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size20,
+                horizontal: Sizes.size14,
+              ),
+              itemBuilder: (context, index) {
+                final isMine = index % 2 == 0;
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment:
+                      isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(Sizes.size14),
+                      decoration: BoxDecoration(
+                        color: isMine
+                            ? Colors.blue
+                            : Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(Sizes.size20),
+                          topRight: const Radius.circular(Sizes.size20),
+                          bottomLeft: Radius.circular(
+                              isMine ? Sizes.size20 : Sizes.size5),
+                          bottomRight: Radius.circular(
+                              !isMine ? Sizes.size20 : Sizes.size5),
+                        ),
+                      ),
+                      child: const Text(
+                        'this is a message!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: Sizes.size16,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => Gaps.v10,
+              itemCount: 10,
+            ),
+            Positioned(
+              bottom: 0,
+              width: MediaQuery.of(context).size.width,
+              child: BottomAppBar(
+                color: Colors.grey.shade50,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size14,
+                  vertical: Sizes.size6,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        onTap: _onStartWriting,
+                        minLines: null,
+                        maxLines: null,
+                        textInputAction: TextInputAction.newline,
+                        cursorColor: Theme.of(context).primaryColor,
+                        decoration: InputDecoration(
+                          hintText: 'Send a message...',
+                          hintStyle: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(Sizes.size20),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: Sizes.size12,
+                            vertical: Sizes.size10,
+                          ),
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.only(
+                              right: Sizes.size14,
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: const [
+                                FaIcon(
+                                  FontAwesomeIcons.faceLaugh,
+                                  size: Sizes.size20,
+                                  color: Colors.black,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    if (_isWriting)
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: Sizes.size10,
+                        ),
+                        child: Container(
+                          width: Sizes.size36,
+                          height: Sizes.size36,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey.shade400,
+                          ),
+                          child: const Center(
+                            child: FaIcon(
+                              FontAwesomeIcons.solidPaperPlane,
+                              color: Colors.white,
+                              size: Sizes.size20,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
