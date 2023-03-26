a230cc22f6c514f82a3fcb1787638053886d3b88
JHyun Ahn <jhyunahn226@gmail.com>
2023-03-26 22:54:26 +0900

    #13.2 showDatePicker, showTimePicker, showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -14,22 +14,42 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            onTap: () => showAboutDialog(
-              context: context,
-              applicationVersion: '1.0',
-              applicationLegalese: "All rights reserved. Please don't copy me",
-            ), //앱정보를 보여주는 기본 함수
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              print(date);
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
             title: const Text(
-              'About',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: const Text(
-              'About this app...',
+              'What is your birthday?',
             ),
           ),
-          const AboutListTile(),
+          const AboutListTile(), //위의 ListTile과 완전히
         ],
       ),
     );
