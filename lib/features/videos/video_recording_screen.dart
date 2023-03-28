157650ec07024c3c591e5a6d65ca4ea17fe9b61c
JHyun Ahn <jhyunahn226@gmail.com>
2023-03-28 22:30:48 +0900

    #19.2 CameraController - 카메라 초기화 및 미리보기 표시

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,4 +1,8 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/constants/sizes.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -8,8 +12,92 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool? _hasPermission;
+
+  late final CameraController _cameraController;
+
+  Future<void> initPermissions() async {
+    final camaeraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        camaeraPermission.isDenied || camaeraPermission.isPermanentlyDenied;
+    final micdenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micdenied) {
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    } else {
+      _hasPermission = false;
+      setState(() {});
+    }
+  }
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+
+    if (cameras.isEmpty) return;
+
+    _cameraController =
+        CameraController(cameras[0], ResolutionPreset.ultraHigh);
+    await _cameraController.initialize();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: _hasPermission == null
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Initializig...",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                    Gaps.v20,
+                    CircularProgressIndicator.adaptive(),
+                  ],
+                )
+              : _hasPermission == false
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "You haven't accepted permissios :(",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: Sizes.size20,
+                          ),
+                        ),
+                      ],
+                    )
+                  : _cameraController.value.isInitialized
+                      ? Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            CameraPreview(
+                              _cameraController,
+                            ),
+                          ],
+                        )
+                      : null,
+        ));
   }
 }
