9a46ce5f5261643e9e444f3bf07aaa0774419a61
JHyun Ahn <jhyunahn226@gmail.com>
2023-04-04 21:12:26 +0900

    #25.4 findProfile

--- lib/features/users/models/user_profile_model.dart
+++ lib/features/users/models/user_profile_model.dart
@@ -23,6 +23,14 @@ class UserProfileModel {
         bio = "",
         link = "";
 
+  UserProfileModel.fromJson(Map<String, dynamic> json)
+      : uid = "",
+        email = json["email"],
+        username = json["username"],
+        birthday = json["birthday"],
+        bio = json["bio"],
+        link = json["link"];
+
   Map<String, dynamic> toJson() {
     return {
       "uid": uid,
